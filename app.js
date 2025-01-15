e522fab94cd515e57801b232a8b4b04c2008597c
sahinoor101 <sahinoor101@gmail.com>
2025-01-15 21:42:29 +0530

    Whac A Mole game

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
+let points = 0;
 function clicked(el) {
     el.classList.toggle('hide');
-    document.querySelector(".points").textContent = "1";
+    document.querySelector(".points").textContent = ++points;
 }
 
 const moles = document.querySelectorAll(".mole");
 console.log(moles);
 
 function spawnMoles() {
+    points = 0;
+    timer();
+    document.querySelector(".points").textContent = 0;
+    let t = setInterval(() => {
+        let rand = Math.floor(Math.random() * 6)
+        moles[rand].classList.toggle("hide");
+    }, 250);
+    setInterval(() => {
+        let rand = Math.floor(Math.random() * 6)
+        moles[rand].classList.add("hide");
+    }, 250);
+    setInterval(() => {
+        let rand = Math.floor(Math.random() * 6)
+        moles[rand].classList.add("hide");
+    }, 250);
+    setTimeout(() => {
+        clearInterval(t);
+        document.querySelector(".score").classList.remove('hide')
+        document.querySelector(".score").textContent = "Your Score:" + points;
 
-        let t =setInterval(() => {
-            let rand = Math.floor(Math.random() * 6)
-            moles[rand].classList.toggle("hide");
-        }, 250);
-        setInterval(() => {
-            let rand = Math.floor(Math.random() * 6)
-            moles[rand].classList.add("hide");
-        }, 250);
-        setInterval(() => {
-            let rand = Math.floor(Math.random() * 6)
-            moles[rand].classList.add("hide");
-        }, 250);
-        setTimeout(() => {
-            clearInterval(t);
-        }, 20000);
-    
+    }, 25000);
+
+}
+function timer() {
+    let sec = 24;
+    let timer = setInterval(function () {
+        document.querySelector('.timer').innerHTML = sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
 }
